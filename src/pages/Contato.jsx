e71a6d436974dc6532303111da2c5b45c90bd7b9
furpe <28751@raphaeldisanto.com.br>
2026-06-11 12:20:23 -0300

    Update Contato.jsx

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -3,13 +3,13 @@ import ContatoGif from '../assets/Rigby.webp'
 import { useNavigate } from 'react-router-dom'
 
 function Contato() {
-  const navigate = useNavigate
+  const navigate = useNavigate()
   return (
     <div className={styles.contato}>
       <h1>Contato</h1>
       <img src={ContatoGif} alt="" className={styles.gif} />
       <button onClick={()=> navigate('/Sobre')}>Sobre</button>
-      <button onClick={()=> navigate('/Home')}>Home</button>
+      <button onClick={()=> navigate('/')}>Home</button>
     </div>
   )
 }
