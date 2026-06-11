16ee3baef49fda3a4fca28c3ad4d5ea7b2a46ac2
furpe <28751@raphaeldisanto.com.br>
2026-06-11 12:19:59 -0300

    Update Sobre.jsx

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -3,12 +3,12 @@ import SobreGif from '../assets/Rocket.webp'
 import { useNavigate } from 'react-router-dom'
 
 function Sobre() {
-  const navigate = useNavigate
+  const navigate = useNavigate()
   return (
     <div className={styles.sobre}>
       <h1>Sobre</h1>
       <img src={SobreGif} alt="" className={styles.gif} />
-      <button onClick={()=> navigate('/Home')} >Home</button>
+      <button onClick={()=> navigate('/')} >Home</button>
       <button onClick={()=> navigate('/Contato')} >Contato</button>
     </div>
   )
